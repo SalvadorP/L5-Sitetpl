00ae235e489291c04e28263d56d7afc93ef6988d
SalvadorP <salvadorperezd@gmail.com>
2017-11-09 12:47:01 +0100

    Added seeders, controllers and backend/frontend pages and behaviour.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Only for dev env!!!
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@localhost.com',
+            'password' => bcrypt('1234'),
+        ]);
+
+        factory(App\User::class, 10)->create();
+       
+    }
+}
